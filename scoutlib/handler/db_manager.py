2b7c2ae88c9996d60a75b3b9387c9df192b1502a
Marcus Grant <marcusfg@gmail.com>
2024-05-15 17:09:11 +0200

    Remove old tests of DBManager

--- scoutlib/handler/db_manager.py
+++ scoutlib/handler/db_manager.py
@@ -1,12 +1,11 @@
 # TODO: Init needs to check for preexisting db & use future CfgRepo to read it before other init steps.
 #       This will also require extra rewrites of init.
-import os
 from pathlib import PurePath as PP
 from typing import Optional, Union
-import sqlite3 as sql
 
 from scoutlib.model.dir import Dir
 from scoutlib.model.file import File
+from scoutlib.handler.db_connector import DBConnector
 from scoutlib.handler.dir_repo import DirRepo
 from scoutlib.handler.file_repo import FileRepo
 
@@ -18,67 +17,19 @@ class DBManager:
     Then it sets up the Repo classes for their associated tables.
     """
 
-    path: PP  # path to the db (can be separate from repo root)
-    root: PP  # path to the repo root in filesystem
+    db: DBConnector  # Database connector class
     dir_repo: DirRepo  # Repo class for the Dir table
     file_repo: FileRepo  # Repo class for the File table
 
+    # TODO: Modify {Dir,File}Repo to accept DBConnector instead of path and root & its methods replace their versions.
     def __init__(
         self,
-        path_db: Union[str, PP],
-        path_fs: Optional[Union[str, PP]] = None,
+        path: Union[PP, str],
+        root: Optional[Union[PP, str]] = None,
     ):
-        # Ensure path_db is a PurePath object
-        if isinstance(path_db, str):
-            path_db = PP(path_db)  # Convert string to PurePath
-
-        if not isinstance(path_db, PP):
-            raise ValueError(f"Invalid path_db: {path_db}")
-        self.path = path_db
-
-        # Handle path_fs and ensure it's a PurePath object
-        if path_fs is None:
-            # Default repo root is the parent of the db path
-            self.root = self.path.parent
-        elif isinstance(path_fs, str):
-            self.root = PP(path_fs)  # Convert string to PurePath
-        elif isinstance(path_fs, PP):
-            self.root = path_fs
-        else:
-            raise ValueError(f"Invalid path_fs: {path_fs}")
-
-        # If no db file exists create it
-        if not os.path.exists(self.path):
-            self._init_db()
-        else:
-            if os.path.isdir(self.path.parent):
-                # If it already exists but no fs_meta table, create it
-                with sql.connect(self.path) as conn:
-                    # Check if the fs_meta table exists
-                    c = conn.cursor()
-                    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-                    if ("fs_meta",) not in c.fetchall():
-                        self._init_db()
-
-        # TODO: Pass reference to self to DirRepo and FileRepo to handle path (de)normalization
+        # Initialize and assign DBConnector
+        self.db = DBConnector(path, root)
 
         # Initialize Repo members
-        self.dir_repo = DirRepo(self.path, self.root)
-        self.file_repo = FileRepo(self.path, self.root)
-
-    def _init_db(self):
-        """Initialize the database with the filesystem table, only necessary one"""
-        with sql.connect(self.path) as conn:
-            try:
-                query = """
-                    CREATE TABLE fs_meta (
-                        property TEXT PRIMARY KEY,
-                        value TEXT
-                    );
-                """
-                conn.cursor().execute(query)
-                query = """INSERT INTO fs_meta (property, value) VALUES ('root', ?)"""
-                conn.cursor().execute(query, (str(self.root),))
-                conn.commit()
-            except sql.Error as e:
-                print("An error occurred:", e)
+        self.dir_repo = DirRepo(self.db.path, self.db.root)
+        self.file_repo = FileRepo(self.db.path, self.db.root)
--- test/handler/test_db_manager.py
+++ test/handler/test_db_manager.py
@@ -1,13 +1,17 @@
-from contextlib import contextmanager
-from unittest.mock import patch
+# from contextlib import contextmanager
+from unittest.mock import patch, MagicMock
 import os
 from pathlib import PurePath as PP
 import pytest
-import sqlite3
+
+# import sqlite3
 import tempfile
-from typing import Optional, Union, Generator
+# from typing import Optional, Union, Generator
 
 from scoutlib.handler.db_manager import DBManager
+from scoutlib.handler.db_connector import DBConnector
+from scoutlib.handler.dir_repo import DirRepo
+from scoutlib.handler.file_repo import FileRepo
 
 
 @pytest.fixture
@@ -25,104 +29,74 @@ def base_repo():
 
 
 class TestInit:
-    """Test cases for init and associated methods of DBManager."""
+    def testInitAssignments(self):
+        # Mock the member objects' init methods
+        pass
 
-    @pytest.mark.parametrize(
-        "path, root",
-        [
-            (1, None),
-            (PP("/a/b/c"), 1),
-            (None, None),
-        ],
-    )
-    def test_args_raise(self, path, root):
-        """Test that these conditions raise a ValueError or TypeError.
-        - path_db is not a PurePath or str
-        - path_fs is not a PurePath or str
-        - parent of path_db is not a dir
-        - path_fs is not a dir
-        """
-        with pytest.raises((ValueError, TypeError)):
-            DBManager(path, root)
 
-    def test_default_root(self):
-        """When no root given, assumes parent dir of db file is root"""
-        # Create fake file
-        fd, path = tempfile.mkstemp(suffix=".db")
-        os.close(fd)
-        # Act
-        db = DBManager(path)
-        # Assert
-        assert db.root == PP(os.path.dirname(path))
-        # Cleanup
-        os.unlink(path)
+# def test_default_root(self):
+#     """When no root given, assumes parent dir of db file is root"""
+#     # Create fake file
+#     fd, path = tempfile.mkstemp(suffix=".db")
+#     os.close(fd)
+#     # Act
+#     db = DBManager(path)
+#     # Assert
+#     assert db.root == PP(os.path.dirname(path))
+#     # Cleanup
+#     os.unlink(path)
 
-    def test_call_init_db_if_not_there(self):
-        """Calls DBManager._init_db if the db does not exist."""
-        # Setup a temp directory using tempfile
-        with tempfile.TemporaryDirectory() as tmpdir:
-            # Setup mock of _init_db
-            with patch("scoutlib.handler.db_manager.DBManager._init_db") as mock_init:
-                DBManager(f"{tmpdir}/scout.db")
-                mock_init.assert_called_once()
+# def test_fs_meta_table_exists(self, base_repo):
+#     """Test that the fs_meta table exists."""
+#     # First check that the file exists
+#     with sqlite3.connect(base_repo.path) as conn:
+#         cursor = conn.cursor()
+#         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+#         tables = cursor.fetchall()
+#         assert ("fs_meta",) in tables
 
-    def test_dont_call_init_db_if_there(self, base_repo):
-        """Test that the db exists and does not call _init_db."""
-        with patch("scoutlib.handler.db_manager.DBManager._init_db") as mock_init:
-            DBManager(base_repo.path)
-            mock_init.assert_not_called()
+# def test_fs_meta_schema(self, base_repo):
+#     # Expected schema is list for every column with tuple of:
+#     # (num: int, name: str, dtype: str, nullable: bool, prime_key: bool)
+#     # Bools are represented as 0|1, but python evaluates them as False|True
+#     expected_schema = [
+#         (0, "property", "TEXT", 0, None, 1),
+#         (1, "value", "TEXT", 0, None, 0),
+#     ]
+#     with sqlite3.connect(base_repo.path) as conn:
+#         cursor = conn.cursor()
+#         cursor.execute("PRAGMA table_info(fs_meta);")
+#         schema = cursor.fetchall()
+#         assert schema == expected_schema
 
-    def test_fs_meta_table_exists(self, base_repo):
-        """Test that the fs_meta table exists."""
-        # First check that the file exists
-        with sqlite3.connect(base_repo.path) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-            tables = cursor.fetchall()
-            assert ("fs_meta",) in tables
+# # TODO: This needs to be parroted on all component repo modules
+# def test_init_db_not_alter_existing_table(self):
+#     """Test that _init_db does not alter existing tables."""
+#     fd, path = tempfile.mkstemp(suffix=".db")
+#     os.close(fd)
+#     with sqlite3.connect(path) as conn:
+#         c = conn.cursor()
+#         c.execute("CREATE TABLE fs_meta (property TEXT PRIMARY KEY, value TEXT);")
+#         c.execute(
+#             "INSERT INTO fs_meta (property, value) VALUES ('root', '/a/b/c');"
+#         )
+#         conn.commit()
+#     # Call DBManager with existing db files
+#     db = DBManager(path)
+#     with sqlite3.connect(db.path) as conn:
+#         cursor = conn.cursor()
+#         cursor.execute("SELECT * FROM fs_meta;")
+#         assert cursor.fetchall() == [("root", "/a/b/c")]
+#     # Cleanup
+#     os.unlink(path)
 
-    def test_fs_meta_schema(self, base_repo):
-        # Expected schema is list for every column with tuple of:
-        # (num: int, name: str, dtype: str, nullable: bool, prime_key: bool)
-        # Bools are represented as 0|1, but python evaluates them as False|True
-        expected_schema = [
-            (0, "property", "TEXT", 0, None, 1),
-            (1, "value", "TEXT", 0, None, 0),
-        ]
-        with sqlite3.connect(base_repo.path) as conn:
-            cursor = conn.cursor()
-            cursor.execute("PRAGMA table_info(fs_meta);")
-            schema = cursor.fetchall()
-            assert schema == expected_schema
+# def test_member_repos(self, base_repo):
+#     """Test that the DirRepo and FileRepo members are created."""
+#     assert base_repo.dir_repo is not None
+#     assert base_repo.dir_repo.path_db == base_repo.path
+#     assert base_repo.dir_repo.root == base_repo.root
+#     assert base_repo.file_repo is not None
 
-    # TODO: This needs to be parroted on all component repo modules
-    def test_init_db_not_alter_existing_table(self):
-        """Test that _init_db does not alter existing tables."""
-        fd, path = tempfile.mkstemp(suffix=".db")
-        os.close(fd)
-        with sqlite3.connect(path) as conn:
-            c = conn.cursor()
-            c.execute("CREATE TABLE fs_meta (property TEXT PRIMARY KEY, value TEXT);")
-            c.execute(
-                "INSERT INTO fs_meta (property, value) VALUES ('root', '/a/b/c');"
-            )
-            conn.commit()
-        # Call DBManager with existing db files
-        db = DBManager(path)
-        with sqlite3.connect(db.path) as conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM fs_meta;")
-            assert cursor.fetchall() == [("root", "/a/b/c")]
-        # Cleanup
-        os.unlink(path)
-
-    def test_member_repos(self, base_repo):
-        """Test that the DirRepo and FileRepo members are created."""
-        assert base_repo.dir_repo is not None
-        assert base_repo.dir_repo.path_db == base_repo.path
-        assert base_repo.dir_repo.root == base_repo.root
-        assert base_repo.file_repo is not None
-
-    # TODO: Test that db already exists and checks mock call for _init_db called
-    # TODO: Test for init raises for wrong args
-    # TODO: Check that _init_db creates config table
+# TODO: Test that db already exists and checks mock call for _init_db called
+# TODO: Test for init raises for wrong args
+# TODO: Check that _init_db creates config table
